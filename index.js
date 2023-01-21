ab85df1a8065c130099b73962a15dd3e8bd14e6b
unknown <joshuarosen117@gmail.com>
2023-01-21 00:11:18 -0800

    Tweaking styling, adding text.

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var width = $(window).width(); 
 window.onscroll = function(){
 if ((width >= 1000)){
-    if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    if(document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
         $("#header").css("background","#fff");
         $("#header").css("box-shadow","0px 0px 20px rgba(0,0,0,0.09)");
-        $("#header").css("padding","4vh 4vw");
+        $("#header").css("padding","3vh 4vw");
     }else{
         $("#header").css("background","transparent");
         $("#header").css("box-shadow","0px 0px 0px rgba(0,0,0,0)");
-        $("#header").css("padding","6vh 4vw");
+        $("#header").css("padding","3vh 4vw");
     }
 }
 }
