9c82a36f5665cf48430e0286e6114ef2c0b5d953
unknown <joshuarosen117@gmail.com>
2023-01-21 12:06:38 -0800

    Adding contact icons.

--- index.js
+++ index.js
@@ -13,24 +13,6 @@ if ((width >= 1000)){
 }
 }
 
-function magnify(imglink){
-    $("#img_here").css("background",`url('${imglink}') center center`);
-    $("#magnify").css("display","flex");
-    $("#magnify").addClass("animated fadeIn");
-    setTimeout(function(){
-        $("#magnify").removeClass("animated fadeIn");
-    },800);
-}
-
-function closemagnify(){
-    $("#magnify").addClass("animated fadeOut");
-    setTimeout(function(){
-        $("#magnify").css("display","none");
-        $("#magnify").removeClass("animated fadeOut");
-        $("#img_here").css("background",`url('') center center`);
-    },800);
-}
-
 $(document).ready(function(){
     $("a").on('click', function(event) {
       if (this.hash !== "") {
