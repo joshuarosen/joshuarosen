4e6c6e3b19974965d02f36c416789e67524ad138
unknown <joshuarosen117@gmail.com>
2023-01-21 23:39:52 -0800

    Some cleanup.

--- index.js
+++ index.js
@@ -4,26 +4,10 @@ if ((width >= 1000)){
     if(document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
         $("#header").css("background","#fff");
         $("#header").css("box-shadow","0px 0px 20px rgba(0,0,0,0.09)");
-        $("#header").css("padding","3vh 4vw");
     }else{
         $("#header").css("background","transparent");
         $("#header").css("box-shadow","0px 0px 0px rgba(0,0,0,0)");
-        $("#header").css("padding","3vh 4vw");
     }
 }
 }
-
-$(document).ready(function(){
-    $("a").on('click', function(event) {
-      if (this.hash !== "") {
-        event.preventDefault();
-        var hash = this.hash;
-        $('body,html').animate({
-        scrollTop: $(hash).offset().top
-        }, 1800, function(){
-        window.location.hash = hash;
-       });
-       } 
-      });
-  });
   
