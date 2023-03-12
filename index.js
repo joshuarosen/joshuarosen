d7bca944f5f05154de2e43eaabbba6d4f6473382
unknown <joshuarosen117@gmail.com>
2023-03-11 17:44:25 -0800

    Cleanup unused stuff.

--- index.js
+++ index.js
@@ -1,13 +1,2 @@
-// var width = $(window).width(); 
-// window.onscroll = function(){
-// if ((width >= 1000)){
-//     if(document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
-//         $("#header").css("background","#fff");
-//         $("#header").css("box-shadow","0px 0px 20px rgba(0,0,0,0.09)");
-//     }else{
-//         $("#header").css("background","transparent");
-//         $("#header").css("box-shadow","0px 0px 0px rgba(0,0,0,0)");
-//     }
-// }
-// }
-  
+
+$('html, body').animate({scrollTop:0},100);
